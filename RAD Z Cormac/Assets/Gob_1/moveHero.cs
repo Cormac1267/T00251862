b84bd739dbc64d5d724993d68ba3a7a4cf85a652
STUDENT Cormac O Connor <Cormac.OConnor4@students.ittralee.ie>
2024-10-02 12:52:04 +0100

    Signed-off-by: STUDENT Cormac O Connor <Cormac.OConnor4@students.ittralee.ie>

--- RAD Z Cormac/Assets/Gob_1/moveHero.cs
+++ RAD Z Cormac/Assets/Gob_1/moveHero.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class moveHero : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+        
+       
+
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.position += transform.forward * Time.deltaTime;
+            animator.SetBool("is Running", true); 
+        }
+        if (Input.GetKey(KeyCode.S))
         transform.position += transform.forward * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
