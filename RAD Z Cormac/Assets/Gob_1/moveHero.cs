541dc9d41ad3bcdefc943e189b16640e018ca686
STUDENT Cormac O Connor <Cormac.OConnor4@students.ittralee.ie>
2024-10-03 10:52:26 +0100

    Signed-off-by: STUDENT Cormac O Connor <Cormac.OConnor4@students.ittralee.ie>

--- RAD Z Cormac/Assets/Gob_1/moveHero.cs
+++ RAD Z Cormac/Assets/Gob_1/moveHero.cs
@@ -9,37 +9,57 @@ public class moveHero : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        
-       
+        if (animator == null)
+            animator = GetComponent<Animator>();
+        else
+            print("Animator Component found");
 
-        
-        
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetBool("isRunning", false);
         if (Input.GetKey(KeyCode.W))
         {
 
             transform.position += transform.forward * Time.deltaTime;
-            animator.SetBool("is Running", true); 
+            animator.SetBool("isRunning", true);
         }
         if (Input.GetKey(KeyCode.S))
-        transform.position += transform.forward * Time.deltaTime;
+            transform.position += transform.forward * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.up, 30*Time.deltaTime);
-        
+            transform.Rotate(Vector3.up, 30 * Time.deltaTime);
+
         if (Input.GetKey(KeyCode.D))
             transform.Rotate(Vector3.down, 30 * Time.deltaTime);
-        
-        if (Input.GetKey(KeyCode.E))
-            transform.position += Vector3.back * Time.deltaTime; 
 
-        
-            
+        if (Input.GetKey(KeyCode.E))
+            transform.position += Vector3.back * Time.deltaTime;
+
+
+
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collision.gameObject.name");
+        collision.gameObject.transform.position += Vector3.forward;
+        FootballScript myFootball = collision.gameObject.GetComponent<FootballScript>();
+        if (myFootball != null)
+        {
+            myFootball.kick();
+        }
 
     }
 }
+        
+
+
